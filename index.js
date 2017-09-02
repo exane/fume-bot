24f084cb75e3b01522412542938f58eb50750da6
Tim Meier <raco0n@gmx.de>
2017-09-02 12:23:41 +0000

    Init

---
+++ index.js
@@ -0,0 +1,70 @@
+const kanbanery = require("./kanbanery_feed")
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+const DISCORD_CHANNEL = process.env.DISCORD_CHANNEL
+
+const KANBANERY_API_KEY = process.env.KANBANERY_API_KEY
+const KANBANERY_BOARD_URL = process.env.KANBANERY_BOARD_URL
+const FETCH_INTERVAL = (process.env.FETCH_INTERVAL || 5 * 60) * 1000 // default 5 minutes
+
+const URL = `${KANBANERY_BOARD_URL}/log/?key=${KANBANERY_API_KEY}`
+console.log("Flox-Bot start up")
+console.log("RSS read interval: %d minutes (%d seconds)", FETCH_INTERVAL / 60 / 1000, FETCH_INTERVAL / 1000)
+console.log("Using Kanbanery board: %s", URL)
+
+const emotes = {}
+
+const run = async () => {
+  // init cache
+  await kanbanery.fetch(URL)
+
+  setInterval(async () => {
+    console.log("fetching...")
+    send(await kanbanery.fetch(URL))
+  }, FETCH_INTERVAL)
+}
+
+const send = (feeds) => {
+  feeds.forEach(feed => {
+    const msg = `:triangular_flag_on_post: **${feed.title}**\n${feed.description}\n:point_right: ${feed.task}\n---------------------------------------------------------------------------`
+    client.channels.find("name", DISCORD_CHANNEL).send(msg)
+  })
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+
+  try {
+    run()
+  } catch (e) {
+    console.error("Fetching failed. Reason: ", e)
+  }
+})
+
+client.on("message", async (msg) => {
+  emotes.feelsGoodMan = emotes.feelsGoodMan || client.emojis.find("name", "feelsGoodMan").toString()
+  emotes.feelsBadMan = client.emojis.find("name", "feelsBadMan").toString()
+
+  if (msg.channel.name !== DISCORD_CHANNEL || msg.author.bot === true) return
+  const content = msg.content
+  console.log("Command recognized: %s", content)
+
+  if (content === "!fetch") {
+    msg.channel.send(`yo gonna check it out ${emotes.feelsGoodMan}`)
+
+    const feed = await kanbanery.fetch(URL)
+    if (feed.length === 0) {
+      msg.channel.send(`no mah' dude, nothin' new 'ere ${emotes.feelsBadMan}`)
+      return
+    }
+    send(feed)
+  }
+
+  if (content.match(/^say/)) {
+    msg.channel.send(content.replace(/^say/, ""))
+  }
+})
+
+client.login(DISCORD_TOKEN)
---
+++ kanbanery_feed.js
@@ -0,0 +1,50 @@
+const request = require("request-promise-native")
+const cheerio = require("cheerio")
+
+let _cache = []
+const cache = (feed = []) => {
+  _cache = [..._cache, ...feed]
+
+  // clear cache from previous days
+  _cache = _cache.filter(c => c.date === new Date().toDateString())
+}
+
+const onlyNewFeed = (feed = []) => {
+  return feed.map(f => {
+    const in_cache = _cache.some(c => c.title === f.title && c.time === f.time && c.description === f.description)
+
+    if (in_cache) return false
+    return f
+  }).filter(f => f)
+}
+
+const fetch = async (url) => {
+  const req = await request(url)
+  const $ = cheerio.load(req)
+  const feed = $("#project-history > dd.loaded")
+  const json_feed = onlyNewFeed(feed2json($, feed))
+  console.log("New feed: ")
+  console.log(json_feed)
+  cache(json_feed)
+
+  return json_feed
+}
+
+const feed2json = ($, html) => {
+  const list = $(html).find("li")
+
+  return list.map((i, e) => {
+    return {
+      avatar: $(e).find(".avatar img").attr("src"),
+      title: $(e).find(".text h5").text(),
+      description: $(e).find(".text p").text(),
+      task: $(e).find(".text .time a").attr("href"),
+      time: $(e).find(".text .time").text(),
+      date: new Date().toDateString()
+    }
+  }).get()
+}
+
+module.exports = {
+  fetch
+}
