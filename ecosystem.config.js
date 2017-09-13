1ccb029fc6b288a3687b5c7f4d6f56fc204e75ad
Tim Meier <raco0n@gmx.de>
2017-09-13 11:26:25 +0000

    drop staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,21 +20,13 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy : {
-    staging : {
-      user : "pi",
-      host : "192.168.1.109",
-      ref  : "origin/staging",
-      repo : "https://github.com/exane/fume-bot.git",
-      path : "/home/pi/fume-bot-staging",
-      "post-deploy" : "source /home/pi/fume-bot-staging/shared/env && yarn; pm2 delete ecosystem.config.js --env production; pm2 start ecosystem.config.js --env production"
-    },
     production : {
       user : "pi",
       host : "192.168.1.109",
       ref  : "origin/master",
       repo : "https://github.com/exane/fume-bot.git",
       path : "/home/pi/fume-bot-production",
-      "post-deploy" : "source /home/pi/fume-bot-production/shared/env && yarn; pm2 delete ecosystem.config.js --env production; pm2 start ecosystem.config.js --env production"
+      "post-deploy" : "source /home/pi/fume-bot-production/shared/env && yarn --prod; pm2 delete ecosystem.config.js --env production; pm2 start ecosystem.config.js --env production"
     }
   }
 }
