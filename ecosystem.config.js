03d3f420696a515e472004910b9864b40ddb9dcf
Tim Meier <raco0n@gmx.de>
2018-04-22 17:44:49 +0200

    Migrate to new domain

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,10 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [
-
-    // First application
     {
       name      : "Fume-bot",
       script    : "index.js",
+      exec_interpreter: "/home/exane/.nvm/versions/node/v8.11.1/bin/node",
       env_production : {
         NODE_ENV: "production"
       }
@@ -21,15 +20,14 @@ module.exports = {
    */
   deploy: {
     production: {
-      user: "pi",
+      user: "exane",
       host: [{
-        host: "exane.cf",
-        port: "60022"
+        host: "exane.cf"
       }],
       ref: "origin/master",
       repo: "https://github.com/exane/fume-bot.git",
-      path: "/home/pi/fume-bot",
-      "post-deploy": "source /home/pi/fume-bot/shared/env && yarn --prod; pm2 delete ecosystem.config.js --env production; pm2 start ecosystem.config.js --env production"
+      path: "/home/exane/fume-bot",
+      "post-deploy": "source /home/exane/fume-bot/shared/env && yarn --prod; pm2 delete ecosystem.config.js --env production; pm2 start ecosystem.config.js --env production"
     }
   }
 }
